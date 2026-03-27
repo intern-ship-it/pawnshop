6a59202507610f54c9b555bf48ead70068284f46
intern <intern@graspsoftwaresolutions.com>
2026-03-27 17:29:26 +0530

    feat: add migration to create `temp_card_data` table.

--- backend/database/migrations/2026_03_27_150908_create_temp_card_data_table.php
+++ backend/database/migrations/2026_03_27_150908_create_temp_card_data_table.php
@@ -12,20 +12,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('temp_card_data', function (Blueprint $table) {
-            $table->id();
-            $table->longText('data')->nullable();
-            $table->timestamps();
-        });
-
-        // Uncomment the section below if you need to insert default/test data during migration
-        /*
-        DB::table('temp_card_data')->insert([
-            'data' => '{"Addr1":"NO 6 (LOT 10043)","Addr2":"JALAN AWAN BESAR","Addr3":"TAMAN YARL","BirthPlace":"W. PERSEKUTUAN(KL)","City":"KUALA LUMPUR","DOB":"04\/03\/1951","ERR_CODE":"OK","Gender":"L","ICNo":"510304145317","Left_FP_Verify_Status":"X","Name":"JOE BIDEN","Nationality":"WARGANEGARA","OldICNo":"6456783","Photo":"","Postcode":"58200","Race":"CINA","RegDate":"20191101","Religion":"BUDDHA","Right_FP_Verify_Status":"X","State":"W. PERSEKUTUAN(KL)","Version":"03"}',
-            'created_at' => now(),
-            'updated_at' => '2026-03-27 06:45:26',
-        ]);
-        */
+        if (!Schema::hasTable('temp_card_data')) {
+            Schema::create('temp_card_data', function (Blueprint $table) {
+                $table->id();
+                $table->longText('data')->nullable();
+                $table->timestamps();
+            });
+        }
     }
 
     /**
